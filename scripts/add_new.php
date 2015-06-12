d11c0f24dcb1a37f41e0b5edf71ab0aee42f5a2d
cabrera-dcc <danic.cabrera@gmail.com>
2015-06-12 16:40:54 +0200

    Tabulations removed (b1r20150612)

--- scripts/add_new.php
+++ scripts/add_new.php
@@ -20,40 +20,37 @@ else{
 	$line = fgets($f_old);
 	fputs($f_new,$line);
 
-	fputs($f_new,"\t");
 	fputs($f_new,"<noticia>");
-	fputs($f_new,"\n\t\t");
+	fputs($f_new,"\n");
 	fputs($f_new,"<fecha>");
-	fputs($f_new,"\n\t\t\t");
+	fputs($f_new,"\n");
 	fputs($f_new,date('d/m/Y'));
-	fputs($f_new,"\n\t\t");
+	fputs($f_new,"\n");
 	fputs($f_new,"</fecha>");
-	fputs($f_new,"\n\t\t");
-
-
-	fputs($f_new,"<hora>");
-	fputs($f_new,"\n\t\t\t");
-	fputs($f_new,date('h:i'));
-	fputs($f_new,"\n\t\t");
-	fputs($f_new,"</hora>");
-	fputs($f_new,"\n\t\t");
-
-	fputs($f_new,"<texto>");
-	fputs($f_new,"\n\t\t\t");
-	fputs($f_new,$_POST['texto']);
-	fputs($f_new,"\n\t\t");
-	fputs($f_new,"</texto>");
-	fputs($f_new,"\n\t\t");
-
-	fputs($f_new,"<autor>");
-	fputs($f_new,"\n\t\t\t");
-	fputs($f_new,$_POST['autor']);
-	fputs($f_new,"\n\t\t");
-	fputs($f_new,"</autor>");
-	fputs($f_new,"\n\t");
-	fputs($f_new,"</noticia>");
 	fputs($f_new,"\n");
 
+	fputs($f_new,"<hora>");
+	fputs($f_new,"\n");
+	fputs($f_new,date('h:i'));
+	fputs($f_new,"\n");
+	fputs($f_new,"</hora>");
+	fputs($f_new,"\n");
+
+	fputs($f_new,"<texto>");
+	fputs($f_new,"\n");
+	fputs($f_new,$_POST['texto']);
+	fputs($f_new,"\n");
+	fputs($f_new,"</texto>");
+	fputs($f_new,"\n");
+
+	fputs($f_new,"<autor>");
+	fputs($f_new,"\n");
+	fputs($f_new,$_POST['autor']);
+	fputs($f_new,"\n");
+	fputs($f_new,"</autor>");
+	fputs($f_new,"\n");
+	fputs($f_new,"</noticia>");
+	fputs($f_new,"\n");
 
 	while(!feof($f_old)){
 		$line = fgets($f_old);
@@ -66,5 +63,4 @@ else{
 	rename("noticias.nuevo.txt","noticias.xml");
 
 	header("Location: ../index.php?r=OK");
-
 }
